a9e1a0900cc2558d18fe14484e97c4433acbfa43
Javier Ciberman Mora <jhm.ciberman@gmail.com>
2024-12-08 09:50:11 -0300

    docs

--- Architectus/Layouts/StackLayout.cs
+++ Architectus/Layouts/StackLayout.cs
@@ -7,74 +7,37 @@ namespace Architectus.Layouts;
 
 public enum Orientation
 {
-    Horizontal,
-    Vertical,
+    /// <summary>
+    /// The children are arranged in a row, from left to right.
+    /// </summary>
+    Row,
+
+    /// <summary>
+    /// The children are arranged in a column, from top to bottom.
+    /// </summary>
+    Column,
+
+    /// <summary>
+    /// The children are arranged in a row, from right to left.
+    /// </summary>
+    RowReverse,
+
+    /// <summary>
+    /// The children are arranged in a column, from bottom to top.
+    /// </summary>
+    ColumnReverse,
 }
 
 public class StackLayout : ContainerLayout
 {
+    /// <summary>
+    /// Gets or sets the orientation of the layout.
+    /// </summary>
+    public Orientation Orientation { get; set; } = Orientation.Row;
+
+    /// <summary>
+    /// Gets or sets whether the last child should fill the remaining space. If false, the last child will be
+    /// arranged with its desired size.
+    /// </summary>
     public bool LastChildFill { get; set; } = true;
-
-    public Orientation Orientation { get; set; } = Orientation.Vertical;
-
-    public bool Reverse { get; set; } = false;
-
-    private IReadOnlyList<LayoutElement>? _actualChildren;
-
-    protected override Vector2Int MeasureOverride(Vector2Int availableSize)
-    {
-        var size = Vector2Int.Zero;
-
-        for (var i = 0; i < this.Children.Count; i++)
-        {
-            var child = this.Children[i];
-            var childSize = child.Measure(availableSize);
-
-            if (this.Orientation == Orientation.Horizontal)
-            {
-                size.X += childSize.X;
-                size.Y = Math.Max(size.Y, childSize.Y);
-            }
-            else
-            {
-                size.X = Math.Max(size.X, childSize.X);
-                size.Y += childSize.Y;
-            }
-        }
-
-        return size;
-    }
-
-
-    protected override RectInt ArrangeOverride(RectInt finalRect)
-    {
-        var offset = finalRect.Position;
-
-        this._actualChildren ??= this.Reverse
-            ? this.Children.ToArray().Reverse().ToList()
-            : this.Children;
-
-        for (var i = 0; i < this._actualChildren.Count; i++)
-        {
-            var child = this._actualChildren[i];
-            var fill = this.LastChildFill && i == this._actualChildren.Count - 1;
-
-            if (this.Orientation == Orientation.Horizontal)
-            {
-                var childWidth = fill ? finalRect.Right - offset.X : child.DesiredSize.X;
-
-                child.Arrange(new RectInt(offset, new Vector2Int(childWidth, finalRect.Height)));
-                offset.X += child.Bounds.Width;
-            }
-            else
-            {
-                var childHeight = fill ? finalRect.Bottom - offset.Y : child.DesiredSize.Y;
-
-                child.Arrange(new RectInt(offset, new Vector2Int(finalRect.Width, childHeight)));
-                offset.Y += child.Bounds.Height;
-            }
-        }
-
-        return finalRect;
-    }
 }
