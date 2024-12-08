d73bd159aa5e5563d3fe02163dab4fc67b88731e
Javier Ciberman Mora <jhm.ciberman@gmail.com>
2024-12-08 09:56:16 -0300

    docs

--- Architectus/Layouts/ContainerLayout.cs
+++ Architectus/Layouts/ContainerLayout.cs
@@ -3,10 +3,17 @@ using System.Numerics;
 
 namespace Architectus.Layouts;
 
+/// <summary>
+/// Represents a layout element that contains multiple children.
+/// </summary>
 public abstract class ContainerLayout : LayoutElement
 {
+    /// <summary>
+    /// Gets the children of the container. Adding or removing children should be done through this property.
+    /// </summary>
     public List<LayoutElement> Children { get; } = new List<LayoutElement>();
 
+    /// <inheritdoc/>
     public override void UpdateWorldMatrix(Matrix3x2 parentMatrix)
     {
         base.UpdateWorldMatrix(parentMatrix);
@@ -17,6 +24,7 @@ public abstract class ContainerLayout : LayoutElement
         }
     }
 
+    /// <inheritdoc/>
     public override void Imprint(HouseLot house)
     {
         foreach (var child in this.Children)
