cdae6440a25ea3d64e6c411c2246d164d7e251f5
Javier Ciberman Mora <jhm.ciberman@gmail.com>
2024-12-12 02:35:56 -0300

    Add docs

--- Architectus/Layouts/RoomElement.cs
+++ Architectus/Layouts/RoomElement.cs
@@ -5,12 +5,20 @@ namespace Architectus.Layouts;
 
 public class RoomElement : LayoutElement
 {
+    /// <summary>
+    /// Gets or sets the minimum size of the room.
+    /// </summary>
     public Vector2Int MinSize { get; set; } = Vector2Int.One;
 
+    /// <summary>
+    /// Gets or sets the maximum size of the room.
+    /// </summary>
     public Vector2Int MaxSize { get; set; } = Vector2Int.MaxValue;
 
+    /// <summary>
+    /// Gets or sets the type of the room.
+    /// </summary>
     public RoomType Type { get; set; } = RoomType.Garden;
-
 
     protected override Vector2Int MeasureOverride(Vector2Int availableSize)
     {
@@ -19,6 +27,7 @@ public class RoomElement : LayoutElement
 
     protected override RectInt ArrangeOverride(RectInt finalRect)
     {
+        finalRect.Size = Vector2Int.Max(finalRect.Size, this.MinSize);
         finalRect.Size = Vector2Int.Min(finalRect.Size, this.MaxSize);
 
         return finalRect;
